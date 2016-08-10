82ef8dad90526486625529a8cd68758d2710bda4
Nicolas Ribes <nicolasribes.pro@gmail.com>
2016-08-10 12:12:28 +0200

    [V1.1] releasing v1.1 | breaking change

--- tools/export.sh
+++ tools/export.sh
@@ -4,4 +4,5 @@ mkdir dist/css/
 rm -rf dist/sass/
 mkdir dist/sass/
 cp web/assets/css/slimgrid.css dist/css/
-cp -r src/sass/Slimgrid/ dist/sass/
+cp -r src/sass/Slimgrid/ dist/sass/Slimgrid
+cp -r src/sass/slimgrid.scss dist/sass/
